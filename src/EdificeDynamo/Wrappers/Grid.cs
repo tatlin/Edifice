d2995f5c79f57792bba97ec7c7d992c40d9581d5
Matt Jezyk <matt.jezyk@gmail.com>
2015-06-16 09:39:15 -0400

    updated grid to use new viz manager

--- src/EdificeDynamo/Wrappers/Grid.cs
+++ src/EdificeDynamo/Wrappers/Grid.cs
@@ -40,17 +40,24 @@ namespace EdificeDynamo.Wrappers
             return new Grid(parameters);
         }
 
-        public void Tessellate(IRenderPackage package, double tol = -1, int maxGridLines = 512)
+
+        public void Tessellate(IRenderPackage package, TessellationParameters parameters)
         {
             var el = InternalElement as EdificeCore.Grid;
             if (el == null) return;
 
             // draw the grid
-            package.PushLineStripVertex(el.Start.X, el.Start.Y, el.Start.Z);
-            package.PushLineStripVertex(el.End.X, el.End.Y, el.End.Z);
-            package.PushLineStripVertexCount(2);
-            package.PushLineStripVertexColor(100, 100, 100, 255);
-            package.PushLineStripVertexColor(100, 100, 100, 255);
+            package.AddTriangleVertex(el.Start.X, el.Start.Y, el.Start.Z);
+            package.AddTriangleVertex(el.End.X, el.End.Y, el.End.Z);
+            package.AddTriangleVertex(el.Start.X, el.Start.Y, el.End.Z);
+            //package.
+
+            
+            package.AddLineStripVertex(el.Start.X, el.Start.Y, el.Start.Z);
+            package.AddLineStripVertex(el.End.X, el.End.Y, el.End.Z);
+            package.AddLineStripVertexCount(2);
+            package.AddLineStripVertexColor(100, 100, 100, 255);
+            package.AddLineStripVertexColor(100, 100, 100, 255);
         }
 
         public override string ToString()
